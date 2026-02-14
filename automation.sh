92b27602ca5c24b30115f31279004b348724acd0
Abdullah Slimani <abdullahslimani98@gmail.com>
2026-02-15 10:42:56 +1100

    updates

--- automation.sh
+++ automation.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-cd /home/aboud/programming/retail-data-project
+$PROJECT_DIR = /home/aboud/programming/retail-data-project
+
+cd $PROJECT_DIR 
 
 . .venv/bin/activate
 
 cd retail_scraper
 
-scrapy crawl mmafightstore-wrestling-shoes
+scrapy crawl mmafightstore-wrestling-shoes -s LOG_FILE=$PROJECT_DIR/logs/scrapy.logs
 
