2334024c37f1b1e35f1c70ae422c0c8bb83832ce
Abdullah Slimani <abdullahslimani98@gmail.com>
2026-02-19 10:05:51 +1100

    updates

--- retaildbt/models/silver/silver_runningwarehouse.sql
+++ retaildbt/models/silver/silver_runningwarehouse.sql
@@ -1,10 +1,17 @@
+WITH size_code_t AS (
+  SELECT
+    model_number,
+    name,
+    link,
+    colour,
+    price,
+    UNNEST(sizes_raw_json.style_69879) AS size,
+    scraped_at
+  FROM read_json_auto('gs://retail-data-bucket-01/running-warehouse/*.json')
+)
 SELECT
-  model_number,
-  name,
-  link,
-  colour,
-  price,
-  UNNEST(sizes_raw_json.style_69879) AS size,
-  scraped_at
-FROM read_json_auto('gs://retail-data-bucket-01/running-warehouse/*.json')
+
+
+LEFT JOIN {{ ref('shoe_size') }} s
+ON r.size = s.code 
 
